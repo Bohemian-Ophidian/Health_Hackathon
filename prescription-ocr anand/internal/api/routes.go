4510f699c28fd24f68a664b599b36b92f87ef7f4
PheniX <aadityaa5000@gmail.com>
2025-02-12 22:49:55 +0530

    Trying

---
+++ prescription-ocr anand/internal/api/routes.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"github.com/Aanandvyas/prescription-ocr/internal/api/handlers"
+	"github.com/Aanandvyas/prescription-ocr/internal/models"
+	"net/http"
+)
+
+func SetupRouter(model *models.MedicationModel) http.Handler {
+
+	medicationHandler := handlers.NewMedicationHandler(model)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/medications", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			medicationHandler.CreateMedication(w, r)
+		case http.MethodGet:
+			medicationHandler.GetMedication(w, r)
+		case http.MethodPut:
+			medicationHandler.UpdateMedication(w, r)
+		case http.MethodDelete:
+			medicationHandler.DeleteMedication(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	return mux
+}
