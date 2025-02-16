a3ff7c5019fb62825e8632dd63eaeafd643f6417
PheniX <aadityaa5000@gmail.com>
2025-02-16 21:48:56 +0530

    testing

--- prescription-ocr/internal/services/ocr/tesseract.go
+++ prescription-ocr/internal/services/ocr/tesseract.go
@@ -10,6 +10,9 @@ func ExtractTextFromImage(imagePath string) string {
 	client := gosseract.NewClient()
 	defer client.Close()
 
+	// Set the path to the Tesseract executable if it's not in your system PATH
+	client.SetTesseractPath("C:\\Program Files\\Tesseract-OCR\\tesseract.exe") // Adjust the path if necessary
+
 	err := client.SetImage(imagePath)
 	if err != nil {
 		log.Fatal(err)
